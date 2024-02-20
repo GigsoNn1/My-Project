fd06edd97c8d72a4ac85f4132971490db53a3793
GigsoNn1 <giga.gilauri2007@gmail.com>
2024-02-20 16:45:09 +0400

    Uptade0.2

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const x = 25;
 const y = -25;
 console.log(+x); // expected output: 25
 console.log(+y); // expected output: -25
+console.log(+x); // expected output: 25
+console.log(+y); // expected output: -25
 console.log(+''); // expected output: 0
